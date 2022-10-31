4116a8fa726ead1c7e612327a46c49fb60eaf550
Lauri Mauranen <lauri.mauranen@onebyte.fi>
2022-10-31 11:18:25 +0200

    ring lisätty

--- src/clj/pingpong/server.clj
+++ src/clj/pingpong/server.clj
@@ -1,9 +1,13 @@
 (ns pingpong.server
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "JEEJEEJEE"})
+
+(defn app (-> handler
+              wrap-reload))
 
 (jetty/run-jetty handler {:port 3000})
